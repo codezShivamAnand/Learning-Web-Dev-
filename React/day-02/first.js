17147ec5d0c5a1448fb6706acdbdfe930002abf5
Shivam Anand <proff.shivam17anand@gmail.com>
2025-03-28 15:34:53 +0530

    Learned about NodeMoudles and Package-json

--- React/day-02/first.js
+++ React/day-02/first.js
@@ -1,30 +1,8 @@
-const React = {
-    // styles = {fontSize: "20px", backgroundColor:"blue", color:"white"}
-    createElement: function(tag, styles, children){
-        const element = document.createElement(tag);
-        if(typeof children === 'object'){
-            for(let val of children ){
-                element.append(val);
-            }
-        }
-        else {
-            element.innerText = children;
-        }
-        // we can iterate over styles as we iterate over obj with the help of for in loop 
-        for(let key in styles){
-            // element.style.fontSize = "40px";
-            element.style[key] = styles[key];
-        }
-        return element;
-    }
-}
-
 const element = React.createElement('h1',{fontSize: "40px", backgroundColor:"blue", color:"white"},"hello, sab");
 const element2 = React.createElement('h1',{fontSize: "40px", backgroundColor:"blue", color:"white"},"hello, kaise ho");
+const div1 = React.createElement('div', {}, [element, element2]);
 
-const ReactDOM = {
-    render : function(element, root){   
-        root.append(element);
-    }
-}
-ReactDOM.render(element, document.getElementById("root"));
+const ReactRoot = ReactDOM.createRoot(document.getElementById('root'));
+ReactRoot.render(div1);
+// rendering se jab tak all the things of element , doesnt get laoded, tab tak dusra event listen initiate hi nhi hoga even if u clidk other buttton 
+
---
+++ React/day-02/readme.md
@@ -0,0 +1,10 @@
+<!-- Production ready code i.e, live website:
+  Comment removed 
+  react and react-dom ke portions hata do which u not using 
+  KEEP FILE SIZE LESS AS POSSIBLE  -->
+
+  <!-- BUNDLER : optimises, comment remove kar deta hai, react and reactdom package se jo use portion hai sirf uska use karta hai
+   bundler has access to HTML, CSS , JS CODE AND bundler DOES OPTIMISATION  
+   
+   Bundler: Webpack, vite, parcel 
+   -->
