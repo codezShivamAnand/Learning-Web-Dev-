6e10bfd1d46bfd1149cda9aaff89e7b2c94dd194
Shivam Anand <proff.shivam17anand@gmail.com>
2025-04-26 23:00:54 +0530

    right way to use .cjs

---
+++ Backend/day02/first.js
@@ -0,0 +1,6 @@
+const {sum,sub,mul} = require("./calculate");// nodeJs me by default 'index.js' (if present inside the Calculate file)ko export karwayega agar sirf file mentioned hai toh 
+sum(2,4);
+sub(3,4);
+mul(2,1);
+
+console.log("hello");
