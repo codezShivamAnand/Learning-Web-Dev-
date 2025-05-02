27c3cb0200ea8615cfe2c05a1b4fa484b97c8cbc
Shivam Anand <proff.shivam17anand@gmail.com>
2025-05-02 17:47:39 +0530

    with this mini proj got to know about how get and post acctually works, implemented these methods with the help of postmon

--- Backend/Express/Lec-2/bookstore.js
+++ Backend/Express/Lec-2/bookstore.js
@@ -1,3 +1,42 @@
 const express = require('express');
 
 const app = express(); 
+
+const BookStore =[
+    {id:1, name: "Harry Potter", author: "Jk Rowling"},
+    {id:2, name: "Friends", author: "Frnd author"},
+    {id:3, name: "Nexus", author: "RohitNegi"}
+]
+// TREAT ALL THESE METHOD AS A FUNCTION CALL 
+
+// routing in app.get : puri string route match karwata hai then andr ghusta hai 
+app.get("/book", (req,res)=>{ // at this route the user will be getting all the books 
+    res.send(BookStore);
+})
+
+//Parser 
+app.use(express.json());
+
+// now the user wants to acess a particular book, we can handle that with dynamic paramter 
+app.get("/book/:id", (req, res)=>{
+    // console.log(req.params); // { id: '3' } id is string so parseInt 
+    const id = parseInt(req.params.id); // id is number is 
+    // send the book with this id
+    const book =   BookStore.find(books=> books.id === id);
+    // console.log(book);
+
+    // res.send("milgyi");
+    res.send(book);
+}) 
+
+// to post data 
+app.post("/book", (req, res)=>{
+    // console.log(req.body);
+    const newbook = req.body;
+    BookStore.push(newbook);
+    res.send("saved successfully");
+})
+
+app.listen(6000,()=>{
+    console.log("Listening at port 5000");
+})
