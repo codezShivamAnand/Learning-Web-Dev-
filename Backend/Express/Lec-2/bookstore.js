0bca2634e9a8ccfeec4104ea83b7091dc645fc03
Shivam Anand <proff.shivam17anand@gmail.com>
2025-05-02 20:48:38 +0530

    learnt about remaining methods,..PATCH/PUT/DELETE and their working with the help of nodemon

--- Backend/Express/Lec-2/bookstore.js
+++ Backend/Express/Lec-2/bookstore.js
@@ -5,13 +5,23 @@ const app = express();
 const BookStore =[
     {id:1, name: "Harry Potter", author: "Jk Rowling"},
     {id:2, name: "Friends", author: "Frnd author"},
-    {id:3, name: "Nexus", author: "RohitNegi"}
+    {id:3, name: "Nexus", author: "RohitNegi"},
+    {id:4, name: "Kick", author: "Salman"},
+    {id:5, name: "Raja Babu", author: "Govinda"},
+    {id:6, name: "System Design", author: "RohitNegi"}
+
 ]
 // TREAT ALL THESE METHOD AS A FUNCTION CALL 
 
 // routing in app.get : puri string route match karwata hai then andr ghusta hai 
 app.get("/book", (req,res)=>{ // at this route the user will be getting all the books 
-    res.send(BookStore);
+    
+    // localhost:5000/book?author=RohitNegi
+    console.log(req.query); // { author: 'RohitNegi' }
+    
+    const book = BookStore.filter(book => book.author === req.query.author);    
+    // res.send(BookStore);
+    res.send(book);
 })
 
 //Parser 
@@ -37,6 +47,42 @@ app.post("/book", (req, res)=>{
     res.send("saved successfully");
 })
 
-app.listen(6000,()=>{
+// patch 
+app.patch("/book", (req,res)=>{ // req me hi user jo bhi req(through post or patch or put or delete )karta hai woh req.body kar ke access karte hai
+    const id = req.body.id;
+    // console.log(book);
+    const book = BookStore.find( info => info.id===id);
+    
+    // Since patch me, user can modify some of the attributes not all, to har attributes ke  modifications ka check lagana padega 
+    if(req.body.author)
+    book.author = req.body.author;
+
+    if(req.body.name){
+        book.name = req.body.name;
+    } 
+    // console.log(book);
+    res.send("Patch successfully");
+})
+
+//put
+app.put("/book", (req,res)=>{
+    const book = BookStore.find(book => book.id === req.body.id);
+    // put me saare attributes user modify karta hain 
+    book.author = req.body.author;
+    book.name = req.body.name;
+})
+
+// delete
+
+app.delete("/book/:id",(req,res)=>{ //  route : book/2 , means id = 2 
+    // console.log(req.params); // { id: '2' }
+    const id = parseInt(req.params.id);
+    const index = BookStore.findIndex(book => book.id===id);
+    // delete 
+    BookStore.splice(index, 1);
+    res.send("Deleted-data");
+})
+
+app.listen(5000,()=>{
     console.log("Listening at port 5000");
 })
