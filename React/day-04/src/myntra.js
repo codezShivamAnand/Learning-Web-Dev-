31b59c1e4063de26f904b62535fbabeca72331d6
Shivam Anand <proff.shivam17anand@gmail.com>
2025-05-07 23:34:01 +0530

    added a sorting functionality which sorts cards based on prices

--- React/day-04/src/myntra.js
+++ React/day-04/src/myntra.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React,{useState} from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./component/header";
 import Card from "./component/card";
 import arr from "./utils/arr.jsx"; 
 
-function Sort(){
-    return (
-        <button id="filter">Sort</button>
-    )
+function sortedArr(arr, setarr){
+       
+    arr.sort((a,b)=>a.price - b.price);
+    setarr([...arr]);
+    
 }
      
 function App(){
+    
+    let [A, setA] = useState(arr);
+    // function sort(){
+    //     A.sort((a,b)=>a.price - b.price);
+                // setA(A); is wrong cz setA still pointing to the old arr A which is unsorted, cz arr is sorted in heap, but its reference is not changed here so React will see no change of the array and hence wont chnage anyting in UI 
+                // so first make the clone of the arr/obj, using spread opr, then pass it to the setA 
+    //     setA([...A]);
+    // }
+
     return (
         <>
-            <Header/>
+            <Header/>   
+            {/* <button onClick={sort}>Sort</button> */}
+            <button onClick={()=> sortedArr(A,setA)}>Sort</button>
+
             <div className="card-body" style={{display:"flex",gap:"10px", flexWrap:"wrap" }}>
-                <Sort/>
-                
                 {
-                    arr.map((value, index)=> <Card key={index} cloth={value.cloth} offer={value.offer} price={value.price}/>)
+                    A.map((value, index)=> <Card key={index} cloth={value.cloth} offer={value.offer} price={value.price}/>)
                 }
             </div>
         </>
