189691a6a7eff4ea5e816f60c81b3947d95802be
Shivam Anand <proff.shivam17anand@gmail.com>
2025-05-08 16:41:51 +0530

    html file for the project-Password Generator

---
+++ React/day-08-PasswordGenerator/app.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import ReactDOM from "react-dom/client";
+import './styles.css';
+
+function App(){
+    
+    return (
+        <div className="first">
+
+        </div>
+    )
+}
+const ReactRoot = ReactDOM.createRoot(document.getElementById('root'));
+ReactRoot.render(<App/>);
